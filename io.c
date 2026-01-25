e646de05a0f6f8161bd1deab036514c338dd20be
iinob <iinobemail@gmail.com>
2026-01-24 20:23:52 -0500

    moved from cpp to c

---
+++ io.c
@@ -0,0 +1,70 @@
+#define VGABUF 0xb8000
+#define HEIGHT 80
+#define WIDTH 80 // default for qemu. probably shouldn't be hardcoded but it is so suck it.
+#define NMI_disable_bit 1
+
+#define LINE_HORIZONTAL 0xCD
+#define LINE_VERTICAL 0xBA
+#define CORNER_DR 0xC9 // down right
+#define CORNER_DL 0xBB // down left
+#define CORNER_UR 0xC8 // up right
+#define CORNER_UL 0xBC // up left
+
+void printChar(char character, unsigned int posX, unsigned int posY) {
+	unsigned int offset = (posY * WIDTH + posX) * 2;
+    unsigned char* vga = (unsigned char*)VGABUF;
+    vga[offset] = character;
+    vga[offset + 1] = 0x07;
+}
+
+void print(const char string[], unsigned int posX, unsigned int posY) {
+	int i = 0;
+	while (string[i] != '\0') {
+		printChar(string[i], posX + i, posY);
+		i++;
+	}
+}
+
+void printSquare(unsigned int posX, unsigned int posY, unsigned int sqwidth, unsigned int sqheight) { // width is almost double equivalent height, for qemu at least
+    printChar(CORNER_DR, posX, posY);
+    printChar(CORNER_DL, posX + sqwidth, posY);
+    printChar(CORNER_UR, posX, posY + sqheight);
+    printChar(CORNER_UL, posX + sqwidth, posY + sqheight); // define corners
+
+    for (int i = posX + 1; i < posX + sqwidth; i++) { // horizontal
+        printChar(LINE_HORIZONTAL, i, posY); // top line
+        printChar(LINE_HORIZONTAL, i, posY + sqheight); // bottom line
+    }
+    for (int i = posY + 1; i < posY + sqheight; i++) { // vertical
+        printChar(LINE_VERTICAL, posX, i); // left
+        printChar(LINE_VERTICAL, posX + sqwidth, i); // right
+    }
+
+}
+
+void printNum(int number, int posX, int posY) { // only works with 1-2 digit numbers
+	if (number >= 10) {
+		printChar((number / 10) + '0', posX, posY);
+		printChar((number % 10) + '0', posX + 1, posY);
+	} else {
+		printChar('0', posX, posY);
+		printChar(number + '0', posX + 1, posY);
+	}
+}
+
+static __inline unsigned char inb (unsigned short int __port) { // functions to read and write from system ports
+  unsigned char _v;
+  __asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (__port));
+  return _v;
+}
+
+static __inline void outb (unsigned char __value, unsigned short int __port){
+  __asm__ __volatile__ ("outb %b0,%w1": :"a" (__value), "Nd" (__port));
+}
+
+int getSecond() {
+	outb(0x70, (NMI_disable_bit << 7) | (0x00));
+	unsigned char rawSecond = inb(0x71);
+	unsigned int second = ((rawSecond >> 4) *10) + (rawSecond & 0x0F);
+	return second;
+}
---
+++ kernel.c
@@ -0,0 +1,20 @@
+#define VGABUF 0xb8000
+#define HEIGHT 80
+#define WIDTH 80 // default for qemu. probably shouldn't be hardcoded but it is so suck it. // coming back later and reading the comments I wrote at 3 am is fun
+#define NMI_disable_bit 1
+
+#include "io.c"
+
+void main() {
+while (1) {
+	print("kernel loaded!", 0, 0);
+	//outb (0x70, (NMI_disable_bit << 7) | (0x00)); // set cmos (port 0x70) register to seconds (register 0x00)
+	printSquare(6,6,11,4);
+	print("Welcome to", 7, 7);
+	print("iinob OS!", 7, 9);
+	//unsigned char rawSecond = inb(0x71); // read data from port 0x71 (cmos register value)
+	//unsigned int second = ((rawSecond >> 4) * 10) + (rawSecond & 0x0F);
+	printNum(getSecond(), 0, 2); // cast to char and print probably hopefully
+}
+	return;
+}
